aabe063016be894207514225a38f9aff62c9dddb
Mustafa Shahriar Hazary <45937799+shahriar30j@users.noreply.github.com>
2019-09-04 02:47:03 +0600

    Templating with layout
    EJS templating with layouts which reduces repeating coding

--- 00app/routes/admin.js
+++ 00app/routes/admin.js
@@ -10,7 +10,14 @@ const products = [];
 
 // /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
-  res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+  // res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+  res.render('add-product', {
+    pageTitle: 'Add product',
+    path: '/admin/add-product',
+    formCSS: true,
+    productCSS: true,
+    activeProduct: true
+  });
 });
 
 // /admin/add-product => POST
