44e5dd2a52ad3ab34b7191baccffebf847ea055b
Mustafa Shahriar Hazary <45937799+shahriar30j@users.noreply.github.com>
2019-09-04 22:37:42 +0600

    Section 6 COmplete
    Add user save them in temporary variablewith ejs templating

--- project6/app.js
+++ project6/app.js
@@ -1,5 +1,26 @@
- const express = require('express');
+const express = require('express');
+const bodyParser = require('body-parser');
 
- const app = express();
+const app = express();
 
- app.listen(3000);
+const users = [];
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/', (req, res, next) => {
+    res.render('index', { pageTitle: 'Add user' });
+});
+
+app.get('/users', (req, res, next) => {
+    res.render('users', { pageTitle: 'User', users: users });
+});
+
+app.post('/add-user', (req, res, next) => {
+    users.push({name: req.body.username});
+    res.redirect('/users');
+});
+
+app.listen(3000);
