05f68224164f578ea918417abff0fdf82f350f99
Mustafa Shahriar Hazary <45937799+shahriar30j@users.noreply.github.com>
2019-09-16 19:14:33 +0600

    Section 8
    Section 8 , Register Route and add, edit-delete link creation.

--- 00app/models/product.js
+++ 00app/models/product.js
@@ -9,28 +9,27 @@ const pth = path.join(
     'products.json'
 );
 
-const getProductsFromFile = (cb) => {
-    
+const getProductsFromFile = cb => {
+
     fs.readFile(pth, (err, fileContent) => {
         if (err) {
             return cb([]);
         } else {
             cb(JSON.parse(fileContent));
-        }
+        }  
     });
 };
 
 module.exports = class Product {
-    constructor(ttl) {
-        this.title = ttl;
+    constructor(title, imageUrl, description, price) {
+        this.title = title;
+        this.imageUrl = imageUrl;
+        this.description = description;
+        this.price = price;
     }
+
     save() {
-        // products.push(this);
-        // const pth = path.join(
-        //     path.dirname(process.mainModule.filename),
-        //     'data',
-        //     'products.json'
-        // );
+
         getProductsFromFile(products => {
             products.push(this);
             fs.writeFile(pth, JSON.stringify(products), (err) => {
--- 00app/routes/admin.js
+++ 00app/routes/admin.js
@@ -2,17 +2,20 @@ const path = require('path');
 
 const express = require('express');
 
-const productsController = require('../controllers/products');
+const adminController = require('../controllers/admin');
 
 // const rootDir = require('../util/path');
 
 const router = express.Router();
 
 // /admin/add-product => GET
-router.get('/add-product', productsController.getAddProduct);
+router.get('/add-product', adminController.getAddProduct);
+
+// /admin/products => GET
+router.get('/products',adminController.getProducts);
 
 // /admin/add-product => POST
-router.post('/add-product', productsController.postAddProduct);
+router.post('/add-product', adminController.postAddProduct);
 
 module.exports = router;
 // exports.routes = router;
