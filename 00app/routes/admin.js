d4ab013763d22d71b0055fbf0792b18b1f39559a
Mustafa Shahriar Hazary <45937799+shahriar30j@users.noreply.github.com>
2019-09-19 18:37:24 +0600

    Section 9 Complete
    Delete products and add and delete from Cart.

--- 00app/routes/admin.js
+++ 00app/routes/admin.js
@@ -4,25 +4,21 @@ const express = require('express');
 
 const adminController = require('../controllers/admin');
 
-// const rootDir = require('../util/path');
-
 const router = express.Router();
 
 // /admin/add-product => GET
 router.get('/add-product', adminController.getAddProduct);
 
 // /admin/products => GET
-router.get('/products',adminController.getProducts);
+router.get('/products', adminController.getProducts);
 
 // /admin/add-product => POST
 router.post('/add-product', adminController.postAddProduct);
 
-router.get('/edit-product/:productId',adminController.getEditProduct);
+router.get('/edit-product/:productId', adminController.getEditProduct);
 
-router.post('/edit-product',adminController.postEditProduct);
+router.post('/edit-product', adminController.postEditProduct);
 
-router.post('/delete-product',adminController.postDeleteProduct);
+router.post('/delete-product', adminController.postDeleteProduct);
 
 module.exports = router;
-// exports.routes = router;
-// exports.products = products;
