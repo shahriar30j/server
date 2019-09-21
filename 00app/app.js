e34674b99b152730181c565f2ebc773ea9d4525e
Mustafa Shahriar Hazary <45937799+shahriar30j@users.noreply.github.com>
2019-09-21 21:07:45 +0600

    Section 10
    SQL introduction, Insert and View Using SQL dB

--- 00app/app.js
+++ 00app/app.js
@@ -2,16 +2,26 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+
 const errorController = require('./controllers/error');
+const db = require('./util/database');
+
 
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-// const adminData = require('./routes/admin');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+
+// db.execute('SELECT * FROM products')
+//     .then((result) => {
+//         console.log(result[0]);
+//     })
+//     .catch((err) => {
+//         console.log(err);
+//     });
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
---
+++ 00app/util/database.js
@@ -0,0 +1,14 @@
+const mysql = require('mysql2');
+
+const pool = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    database: 'node-complete',
+    password: '1234@1234'
+});
+
+
+// ALTER USER 'root'@'localhost' IDENTIFIED BY 'your new password'; 
+// ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'your new password';
+
+module.exports = pool.promise(); 
