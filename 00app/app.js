ccc9d6dd9ad9570de62eeb1f0c6a1242dab0cfca
Mustafa Shahriar Hazary <45937799+shahriar30j@users.noreply.github.com>
2019-09-24 13:48:20 +0600

    NodeJS Section 11
    Sequelize the shop module
    half done

--- 00app/app.js
+++ 00app/app.js
@@ -4,7 +4,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
-const db = require('./util/database');
+// const db = require('./util/database');
+const sequelize = require('./util/database');
 
 
 const app = express();
@@ -31,4 +32,13 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-app.listen(3000);
+sequelize.sync()
+    .then(result => {
+        // console.log(result);
+        app.listen(3000);
+    })
+    .catch(err => {
+        console.log(err)
+    });
+
+// app.listen(3000);
--- 00app/util/database.js
+++ 00app/util/database.js
@@ -1,14 +1,23 @@
-const mysql = require('mysql2');
+// const mysql = require('mysql2');
 
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'node-complete',
-    password: '1234@1234'
+// const pool = mysql.createPool({
+//     host: 'localhost',
+//     user: 'root',
+//     database: 'node-complete',
+//     password: '1234@1234'
+// });
+
+
+// // ALTER USER 'root'@'localhost' IDENTIFIED BY 'your new password'; 
+// // ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'your new password';
+
+// module.exports = pool.promise(); 
+
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize('node-complete', 'root', '1234@1234',{
+    dialect: 'mysql',
+    host: 'localhost'
 });
 
-
-// ALTER USER 'root'@'localhost' IDENTIFIED BY 'your new password'; 
-// ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'your new password';
-
-module.exports = pool.promise(); 
+module.exports = sequelize;
